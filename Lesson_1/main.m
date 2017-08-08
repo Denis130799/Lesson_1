e1531bd24fd5b216e57ceef62f14c2a0de6dfad3
Denis <denismishchenko13@gmail.com>
2017-08-08 21:13:15 +0300

    Первый коммит. Добавил проект

---
+++ Lesson_1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Lesson_1
+//
+//  Created by Denis on 08.08.17.
+//  Copyright © 2017 Denis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
